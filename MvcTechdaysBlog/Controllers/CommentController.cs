51a71ebee522f3e2f26fcadb831503d080866cda
Tore Lervik <tore.lervik@bekk.no>
2011-09-01 19:20:07 +0200

    lagt til kommentarer

---
+++ MvcTechdaysBlog/Controllers/CommentController.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcTechdaysBlog.Models;
+
+namespace MvcTechdaysBlog.Controllers
+{ 
+    public class CommentController : Controller
+    {
+        private DataService db = new DataService();
+
+        //
+        // GET: /Comment/
+
+        public ViewResult Index()
+        {
+            var comments = db.Comments.Include(c => c.Article);
+            return View(comments.ToList());
+        }
+
+        //
+        // GET: /Comment/Details/5
+
+        public ViewResult Details(int id)
+        {
+            Comment comment = db.Comments.Find(id);
+            return View(comment);
+        }
+
+        //
+        // GET: /Comment/Create
+
+        public ActionResult Create()
+        {
+            ViewBag.ArticleId = new SelectList(db.Articles, "Id", "Title");
+            return View();
+        } 
+
+        //
+        // POST: /Comment/Create
+
+        [HttpPost]
+        public ActionResult Create(Comment comment)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Comments.Add(comment);
+                db.SaveChanges();
+                return RedirectToAction("Index");  
+            }
+
+            ViewBag.ArticleId = new SelectList(db.Articles, "Id", "Title", comment.ArticleId);
+            return View(comment);
+        }
+        
+        //
+        // GET: /Comment/Edit/5
+ 
+        public ActionResult Edit(int id)
+        {
+            Comment comment = db.Comments.Find(id);
+            ViewBag.ArticleId = new SelectList(db.Articles, "Id", "Title", comment.ArticleId);
+            return View(comment);
+        }
+
+        //
+        // POST: /Comment/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(Comment comment)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(comment).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.ArticleId = new SelectList(db.Articles, "Id", "Title", comment.ArticleId);
+            return View(comment);
+        }
+
+        //
+        // GET: /Comment/Delete/5
+ 
+        public ActionResult Delete(int id)
+        {
+            Comment comment = db.Comments.Find(id);
+            return View(comment);
+        }
+
+        //
+        // POST: /Comment/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {            
+            Comment comment = db.Comments.Find(id);
+            db.Comments.Remove(comment);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
--- MvcTechdaysBlog/Controllers/HomeController.cs
+++ MvcTechdaysBlog/Controllers/HomeController.cs
@@ -4,16 +4,51 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using MvcTechdaysBlog.Filters;
+using MvcTechdaysBlog.Models;
 
 namespace MvcTechdaysBlog.Controllers
 {
     [AllowAnonymous]
     public class HomeController : Controller
     {
+        private DataService db = new DataService();
+
         public ActionResult Index()
         {
-            return View();
+            var articles = db.Articles.OrderBy(a => a.Date).ToList();
+            return View(articles);
         }
 
+        public ActionResult Article(string id)
+        {
+            var article = db.Articles.SingleOrDefault(a => a.Url == id);
+
+            if (article != null)
+            {
+                ViewBag.Comments = article.Comments.OrderByDescending(c => c.Date).ToList();
+                return View(article);
+            }
+            
+            throw new HttpException(404, "NotFound");
+        }
+
+        [HttpPost]
+        public ActionResult PostComment(Comment comment)
+        {
+            if (ModelState.IsValid)
+            {
+                comment.Date = DateTime.Now;
+                db.Comments.Add(comment);
+                db.SaveChanges();
+            }
+
+            return PartialView("Comment" ,comment);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
     }
 }
